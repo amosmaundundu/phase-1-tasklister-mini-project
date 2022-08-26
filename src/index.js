781cd2ab5d8ff1b9235d47fdbc89323b965555ca
amosmaundundu <amosmaundu5@gmail.com>
2022-08-26 13:00:56 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+// variales
+const form =document.querySelector('form');
+
+
+//creating a list
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const inputText = e.target.querySelector('#new-task-description').value;
+
+    const ul= e.target.parentNode.querySelector('#tasks');
+    const btn =document.createElement('button');
+    const li =document.createElement('li')
+
+    btn.innerText ='X'
+    li.innerText = `${inputText} `;
+    li.appendChild( btn);
+    ul.append(li);
+
+    form.reset();
+
+    //removing items on list
+        btn.addEventListener('click',(e)=>{
+          e.target.parentNode.remove();
+        });
+  });
